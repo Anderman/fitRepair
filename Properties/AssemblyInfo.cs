b1f6eaedade6f0d87495cb6877d09046e5975f8f
Anderman <thom@kiesewetter.nl>
2012-06-17 16:10:34 +0200

    rename

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleFitreader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GarminFitRepair")]
+[assembly: AssemblyDescription("Garmin Fit Repair / csv convert")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SimpleFitreader")]
+[assembly: AssemblyProduct("GarminFitRepair")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
